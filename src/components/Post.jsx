944e5f0149c21a9cdecd20587cb1e4d71b21db0f
Geraldo Medina <medinnajr@gmail.com>
2023-08-13 12:49:28 -0300

    back in business

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,19 @@
+
+import styles from './Post.module.css'
 export function Post(){
   return(
-    <h1>Hello World</h1>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/im-medina.png" alt="" />
+          <div className={styles.authorInfo}>
+            <strong>Medina</strong>
+            <span>Web Dev</span>
+          </div>
+        </div>
+        <time title="13 de maio às 08:13 "dateTime="2022-05-11 08:13:30">Publicado à 1h</time>
+
+      </header>
+    </article>
   )
 }
